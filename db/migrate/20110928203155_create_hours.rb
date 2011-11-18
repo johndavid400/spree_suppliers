2f399972d07bb883a38b3f377b0c7f768546cb88
johndavid400 <johndavid400@yahoo.com>
2011-11-18 16:44:10 -0600

    [MIGRATIONS]

--- db/migrate/20110928203155_create_hours.rb
+++ db/migrate/20110928203155_create_hours.rb
@@ -1,7 +1,20 @@
 class CreateHours < ActiveRecord::Migration
   def self.up
+    create_table :hours do |t|
+      t.string :monday
+      t.string :tuesday
+      t.string :wednesday
+      t.string :thursday
+      t.string :friday
+      t.string :saturday
+      t.string :sunday
+      t.integer :supplier_id
+
+      t.timestamps
+    end
   end
 
   def self.down
+    drop_table :cities
   end
 end
