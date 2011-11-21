b1bc248a5e8d6459cc13b74e9d46d01e24e0c6ba
johndavid400 <johndavid400@yahoo.com>
2011-11-21 11:18:17 -0600

    DRAGONFLY

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,16 @@
+module ApplicationHelper
+  def dragonfly_app
+    Dragonfly[:images]
+  end
+
+  def sized_image(image, size)
+      if image != nil
+        if image.attachment.to_file != nil
+          image_tag(dragonfly_app.fetch_file(image.attachment.to_file.path).thumb(size).url)
+        end
+      else
+        noimage = File.open(File.join(Rails.root, "public", "images", "noimage", "product.jpg"))
+        image_tag(dragonfly_app.fetch_file(noimage.path).thumb(size).url)
+      end
+    end
+end
