ada5b0b47e780bc42e886ae78ab08b159f1700fc
johndavid400 <johndavid400@yahoo.com>
2011-11-21 11:55:04 -0600

    [SEEDS]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 # Use this file to load your own seed data from extensions.  
 # See the db/seeds.rb file in the Spree core for some ideas on what you can do here.
+#
+Rake::Task["db:load_dir"].invoke( "default" )
+puts "Default data has been loaded"
 
 puts "Create Taxonomy"
 
