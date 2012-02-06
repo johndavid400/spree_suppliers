7465f119468c9ed3bd88ab1e1571d903ce9d0893
johndavid400 <johndavid400@yahoo.com>
2012-02-06 14:45:29 -0600

    specs and models

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe Supplier do
 
-  FactoryGirl.build(:supplier, :name => 'Joe', :title => "Joe's Plumbing")
-
   it { should have_one(:user) }
   it { should have_many(:supplier_invoices) }
   it { should have_many(:images) }
