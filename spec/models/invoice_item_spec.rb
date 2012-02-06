90ed671c5ba15ab2fea141f576588b20b81abe80
johndavid400 <johndavid400@yahoo.com>
2012-02-06 13:53:26 -0600

    added rspec tests

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Supplier do
+describe InvoiceItem do
   it { should belong_to(:supplier_invoice) }
   it { should belong_to(:line_item)}
 end
--- spec/models/supplier_invoice_spec.rb
+++ spec/models/supplier_invoice_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Supplier do
+describe SupplierInvoice do
   it { should belong_to(:supplier_invoice) }
   it { should have_many(:invoice_items) }
 end
