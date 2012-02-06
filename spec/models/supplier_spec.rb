7fe87abd902828931e47934baafb540432e95c31
johndavid400 <johndavid400@yahoo.com>
2012-02-06 14:28:06 -0600

    fix rspec test

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -13,5 +13,5 @@ describe Supplier do
   it { should have_many(:images) }
   it { should have_many(:products) }
 
-  it { should have_many(:taxons).through(:suppliers_taxons) }
+  it { should has_and_belongs_to_many(:taxons) }
 end
