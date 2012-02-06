24231daafc01d71b577b74ca4cca0ecc1bcb2819
johndavid400 <johndavid400@yahoo.com>
2012-02-06 14:50:48 -0600

    changed relationsip in db

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Supplier do
 
-  it { should have_one(:user) }
+  it { should belong_to(:user) }
   it { should have_many(:supplier_invoices) }
   it { should have_many(:images) }
   it { should have_many(:products) }
