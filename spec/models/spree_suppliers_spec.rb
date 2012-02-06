e7ab1082bd9052d63df4adcaa8ed06ecd41c450c
johndavid400 <johndavid400@yahoo.com>
2012-02-06 13:30:08 -0600

    added an rspec test and a validation to include name and title for suppliers

---
+++ spec/models/spree_suppliers_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ClientApplication do
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_uniqueness_of(:title) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:title) }
+
+  it { should have_one(:user) }
+
+  it { should have_many(:supplier_invoices) }
+  it { should have_many(:images) }
+  it { should have_many(:products) }
+
+  it { should have_many_and_belong_to(:taxons) }
+end
