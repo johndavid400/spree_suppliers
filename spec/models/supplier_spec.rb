91f23008af9566e4f1cf5a5749919eb00c098741
johndavid400 <johndavid400@yahoo.com>
2012-02-06 15:03:52 -0600

    removed has_and_belongs_to_many test

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -7,7 +7,4 @@ describe Supplier do
   it { should have_many(:images) }
   it { should have_many(:products) }
 
-  it { should belong_to(:taxons) }
-  it { should have_many(:taxons) }
-#  it { should has_and_belongs_to_many(:taxons) }
 end
