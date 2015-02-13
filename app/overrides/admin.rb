de1c00b286d712c5aac1810137ef035023804d6a
Péter Nagy <antronin@gmail.com>
2015-02-13 23:06:20 +0100

    Modify spree version dependency

--- app/overrides/admin.rb
+++ app/overrides/admin.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/tabs",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_tabs",
                      :name => "suppliers_tabs",
-                     :insert_after => ".*",
+                     :insert_after => "erb[silent]:last-child",
                      :text => "<% if can?(:admin, Spree::Supplier) %><%= tab :suppliers, icon: 'user' %><% end %>",
                      :disabled => false)
