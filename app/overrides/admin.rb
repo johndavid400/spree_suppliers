3523c4cb03c46c790fe2f6f74f9cf375390c38ad
Péter Nagy <antronin@gmail.com>
2015-02-13 23:02:41 +0100

    Updated overrides

--- app/overrides/admin.rb
+++ app/overrides/admin.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/main_menu",
+Deface::Override.new(:virtual_path => "spree/admin/shared/tabs",
                      :name => "suppliers_tabs",
                      :insert_after => ".*",
                      :text => "<% if can?(:admin, Spree::Supplier) %><%= tab :suppliers, icon: 'user' %><% end %>",
